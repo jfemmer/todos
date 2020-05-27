4b46c545e5626b14d8cd90974967f38837693922
femmer <jfemmer.business@gmail.com>
2020-05-27 16:25:17 -0500

    Initial commit

---
+++ src/main/java/com/example/todos/todo/StatusType.java
@@ -0,0 +1,26 @@
+package com.example.todos.todo;
+
+import java.io.Serializable;
+
+public enum StatusType {
+
+    /**
+     *Indicated High Priority Item
+     */
+    HIGH_PRIORITY,
+
+    /**
+     * Indicated Medium Priority Item
+     */
+    MEDIUM_PRIORITY,
+
+    /**
+     * Indicated Low Priority Item
+     */
+    LOW_PRIORITY,
+
+    /**
+     * Status Unknown
+     */
+    UNKNOWN
+}
